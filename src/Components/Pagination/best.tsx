eb7b6ed482bcdcf5067bb5ff55669d23a3cc3c8e
Asif Mustafayev <53805470+Asifdvp@users.noreply.github.com>
2022-07-06 11:48:52 +0400

    pagination complete

--- src/Components/Pagination/best.tsx
+++ src/Components/Pagination/best.tsx
@@ -1,120 +1,82 @@
-import { Pagination ,Box, Link} from '@mui/material'
-import axios from 'axios';
-import React, { FC, useEffect, useState } from 'react';
-import './index.scss'
-const Best:FC<any> = () => {
-    const  [data,setData] = useState([]);
-    const [currentPage,setCurrentPage] = useState(1);
-    const [postsPerPage, setPostsPerPage] = useState(4);
-    useEffect(()=>{
-        const getData = async() =>{
-            const res = await axios.get("http://localhost:3000/data");
-            const data = res.data;
-            setData(data.tbody);
-        };
-        getData();
-    },[]);
-    //sonnuncu post
-const indexOfLastPost = currentPage * postsPerPage;
-const indexOfFirstPost = indexOfLastPost - postsPerPage;
+import { Pagination, Box, Link } from "@mui/material";
+import axios from "axios";
+import React, { FC, useEffect, useState,useRef } from "react";
+import "./index.scss";
+const Best: FC<any> = () => {
+  const leftR=useRef(null);
+const cur = useRef(null);
+  const [data, setData] = useState([]);
+  let [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage, setPostsPerPage] = useState(4);
+  const [dis,setDisabled] = useState(true)
+  const [disR,setDisabledR] = useState(false)
+  useEffect(() => {
+    const getData = async () => {
+      const res = await axios.get("http://localhost:3000/data");
+      const data = res.data;
+      setData(data.tbody);
+    };
+    getData();
+  }, []);
+  //sonnuncu post
+  const indexOfLastPost = currentPage * postsPerPage;
+  const indexOfFirstPost = indexOfLastPost - postsPerPage;
+  const currentPosts = data.slice(indexOfFirstPost, indexOfLastPost);
+  const pageNumbers = [];
+  const MaxNumber =  Math.ceil(data.length / postsPerPage);
+  for (let i = 1; i <= Math.ceil(data.length / postsPerPage); i++) {
+    pageNumbers.push(i);
+  }
 
-const currentPosts = data.slice(indexOfFirstPost,indexOfLastPost);
-    let pageNumber = Math.ceil(Number(data.length)/Number(postsPerPage));
-//     console.log(pageNumber);
-let paginate = (pageNumber:any) =>{
-    setCurrentPage(pageNumber.target.textContent)
-
-const currentLi =  pageNumber.target.parentElement.parentElement;
-const leftBtn = pageNumber.target.parentElement.parentElement.parentElement.children[0];
-// console.log(leftBtn);
-
-const ul = pageNumber.target.parentElement.parentElement.parentElement.children
-const lastLi = pageNumber.target.parentElement.parentElement.parentElement.children.length - 1;
-const rightBtn = ul[lastLi]
-//    if(pageNumber.target.  === leftBtn){
-//     console.log("asif")
-//    }
-// console.log(pageNumber.target.parentElement.children[0].textContent)
-// console.log(leftBtn);
-// console.log(currentLi)
+  const paginate = async (pageNumber:any) =>{
+  setCurrentPage(pageNumber);
+  if(pageNumber ===1){
+  setDisabled(true)
 }
-const pageNumbers = [];
-for (let i = 1; i <= Math.ceil(data.length / postsPerPage); i++) {
-  pageNumbers.push(i);
+else{
+  setDisabled(false)
 }
-console.log(pageNumbers)
+}
+   const left =   () =>{
+   setCurrentPage(currentPage-1)
+   }
+   const right = () =>{
+    setCurrentPage(currentPage +1 )    
+    }
+   useEffect(()=>{
+    if(currentPage ===1 ){
+     setDisabled(true)
+}
+if(currentPage< MaxNumber){
+  setDisabledR(false)
+}
+if(currentPage === MaxNumber){
+  setDisabledR(true)
+ }
+
+   },[currentPage])
+
+ 
   return (
     <div>
-
-
-<Box  sx={{
-
-}}>
-        <Pagination
-        // onClick={()=>paginate()}
-        onClick={paginate}
-          count={pageNumber}
-          shape="rounded"
-          color="primary"
-          sx={{
-            
-      width:"500px",
-            ".MuiButtonBase-root,.MuiPaginationItem-sizeMedium ,.MuiPaginationItem-outlined ,.MuiPaginationItem-rounded, .MuiPaginationItem-outlinedPrimary, .Mui-selected, .MuiPaginationItem-page":
-              {
-                backgroundColor: "white",
-                color: "#00b3fc",
-                borderColor: "#00b3fc",
-                border:"1px solid #00b3fc",
-                "&:hover": {
-                  backgroundColor: "#00b3fc",
-                  color: "white",
-                },
-                "&:focus": {
-                  backgroundColor: "#00b3fc",
-                  color: "white",
-                },
-              },
-          }}
-        /> 
-              {/* <ul className='pagination' >
-                <li>left</li>
-          {pageNumbers.map((number:any) => (
-            <li key={number} className='page-item'>
-              <Link onClick={(e) => {
-                // console.log(e.target.textContent)
-paginate(number)
-              }   }    to="/best" className='page-link' >
-                {number}
-              </Link>
-            </li>
-            <li>next</li>
+      <Box sx={{}}>
+        <ul className="pagination">
+          <button disabled={dis} ref={leftR} className="page-item left"  onClick={left}>Li</button>
+          {pageNumbers.map((num: number) => (
+              <li  className="page-item" key={num} onClick={(e)=>{paginate(num)}}>{num}</li>          
           ))}
-        </ul> */}
-        <ul className='pagination'>
-          <li className='page-item'>Li</li>
-          {pageNumbers.map((num:number)=>(
-            <Link  key={num}  onClick = {(e)=>{console.log('dsfdsf')}}>
-   <li className="page-item">{num}</li>
-            </Link>
-         
-          ))}
-          <li className='page-item'>Right</li>
+          <button  disabled={disR} className="page-item right" onClick={right}>Right</button>
         </ul>
+      </Box>
+      <Box>
+<ul>
+  {currentPosts.map((item:any)=>(
+     <li key={item.id}>{item.id}</li>
+  ))} 
+</ul>
         </Box>
-        {/* <ReactPaginate
-        previousLabel={"Previous"}
-        nextLabel={"Next"}
-        pageCount={pageCount}
-        onPageChange={changePage}
-        containerClassName={"paginationBttns"}
-        previousLinkClassName={"previousBttn"}
-        nextLinkClassName={"nextBttn"}
-        disabledClassName={"paginationDisabled"}
-        activeClassName={"paginationActive"}
-      /> */}
-
     </div>
-  )
-}
+  );
+};
 
-export default Best
+export default Best;
